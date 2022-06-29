d19f8c7d08b1a23e2eea163752eaf7a16ff7539c
ZahraDashti <dashti.zahra@gmail.com>
2022-06-29 16:54:05 +0430

    update helper

--- CustomAttributeExample/AttributeHelper.cs
+++ CustomAttributeExample/AttributeHelper.cs
@@ -8,15 +8,17 @@ namespace CustomAttributeExample
     {
         public static Dictionary<string, object?> FindByAttribute<TRequest, TAttribute>(TRequest request) where TAttribute : Attribute
         {
-            if (request == null)
-                return new Dictionary<string, object?>();
-
-            var props = request.GetType().GetProperties().Where(
-                prop => Attribute.IsDefined(prop, typeof(TAttribute)));
-
             var dict = new Dictionary<string, object?>();
-            foreach (var property in props)
-                dict[property.Name] = property.GetValue(request);
+
+            if (request != null)
+            {
+                dict["Type"] = request.GetType();
+                var props = request.GetType().GetProperties().Where(
+                    prop => Attribute.IsDefined(prop, typeof(TAttribute)));
+
+                foreach (var property in props)
+                    dict[property.Name] = property.GetValue(request);
+            }
 
             return dict;
         }
